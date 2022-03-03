20b68f7d2c3952235cb3bdf71abc31f1b680d486
jhinzhou <jhinzhou@rektec.com.cn>
2022-03-03 14:21:03 +0800

    change build

--- ZUI-TAB/vite.config.js
+++ ZUI-TAB/vite.config.js
@@ -1,9 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from "@vitejs/plugin-vue-jsx"
-import { viteMockServe } from 'vite-plugin-mock'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { viteMockServe } from "vite-plugin-mock";
+import path from "path";
 
-// https://vitejs.dev/config/
+/**
+ * @type {import('vite').UserConfig}
+ */
 export default defineConfig({
-  plugins: [vue(), vueJsx(), viteMockServe({ supportTs: false })]
-})
+  // css: {},
+  // esbuild: {},
+  alias: {
+    "@": path.join(__dirname, "src"),
+    comps: path.join(__dirname, "src/components"),
+    views: path.join(__dirname, "src/views"),
+    routes: path.join(__dirname, "src/router"),
+    styles: path.join(__dirname, "src/styles"),
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    viteMockServe({
+      supportTs: false,
+    }),
+  ],
+});
